3b8f10bb97f31e4f746bfa2d412e3e03fed54665
PHEAPKONGKEA <hongdaray9@gmail.com>
2023-12-02 10:37:37 +0700

    Test Action

---
+++ Test02/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test02.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.name = "Show you home Page";
+            return View();
+        }
+        public ActionResult ShowMoreDetail()
+        {
+            return View("~/Views/About/Index.cshtml");     
+        }
+        public int add()
+        {
+            int a = 3;
+            int b = 4;
+            int s = a + b;
+            return s;
+        }
+        public ActionResult Show()
+        {
+            return File(Server.MapPath("~/Content/test.text"), "text/plain");
+        }
+        public FileResult Download()
+        {
+            return File(Server.MapPath("~/Content/test.text"), "text/plain", "MVC5.txt");
+        }
+    }
+}
---
+++ Test02/Controllers/test1Controller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test02.Controllers
+{
+    public class test1Controller : Controller
+    {
+        // GET: test1
+        public ActionResult Show()
+        {
+            return File(Server.MapPath("~/Content/text.text"),"text/plain");
+        }
+        public FileResult Download()
+        {
+            return File(Server.MapPath("~/Content/test.text"), "text/plain", "MVC5.txt");
+        }
+        public RedirectResult ShowExternal()
+        {
+            return Redirect("https://google.com");
+        }
+    }
+}
